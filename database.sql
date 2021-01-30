7a4b6f885fb0510df3fb46bef2ce498b1bfc707a
alesmo18 <73285425+alesmo18@users.noreply.github.com>
2021-01-30 18:02:36 +0100

    Add files via upload
    Ultima versione script

--- database.sql
+++ database.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE drisk_db
+CREATE DATABASE drisk_db;
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `drisk_db`.`users` (`user_id`, `active`, `email`, `password`, `user_name`) VALUES ('1','1','admin','$2a$10$Ga3PJW1eQWG4QhsPNUoGr.3eAg7rImrxHcXINPbB02cZt29yBSN.e','admin');
